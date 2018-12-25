435cd046ef9887783372149d377c4f39b5702852
cpq <826959531@qq.com>
2018-12-25 20:43:21 +0800

    git

---
+++ mygittest/git_01/src/com/dlc/MyWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.dlc.MyWorld;
+
+public class HelloWorld {
+
+    public void save(){
+        System.out.println("保存");
+    }
+}
+
+class HelloGit{
+    public void delete(){
+        System.out.println("shanchu ");
+    }
+}
